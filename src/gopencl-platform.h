5e8e5403cfb73f28fcdb4aa95f81d8e07e8073b8
Juan R. Garcia Blanco <juanrgar@gmail.com>
2012-02-21 19:59:27 +0100

    a

---
+++ src/gopencl-platform.h
@@ -0,0 +1,14 @@
+//
+//  gopencl-platform.h
+//  
+//
+//  Created by Juan Rafael García Blanco on 2/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef _gopencl_platform_h
+#define _gopencl_platform_h
+
+
+
+#endif
